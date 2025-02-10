eee693d35d7b016ab94492693f00756501fa6629
Quilhar <davidtit240@gmail.com>
2025-02-10 13:17:38 -0500

    coomitS

--- spritesheet/main.py
+++ spritesheet/main.py
@@ -7,11 +7,13 @@ class Game:
     def __init__(self):
         pygame.init()
         pygame.mixer.init()
+        pygame.font.init()
 
         self.screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
         self.load_images()
+        self.playing = False
 
     def load_images(self):
         '''load and get all images'''
@@ -27,8 +29,7 @@ class Game:
                 tile_x = 16
                 tile_y = 16
                 
-                tile_image = tile_sheet.get_image(locx, locy, tile_x, tile_y, 4, 4)
-                tile_image.set_colorkey(BLACK)
+                tile_image = tile_sheet.get_image(locx, locy, tile_x, tile_y, 2, 2, True)
                 self.tile_list.append(tile_image)
 
 
@@ -36,8 +37,15 @@ class Game:
         self.grass_list = [self.tile_list[0], self.tile_list[1], self.tile_list[2]]
         self.dirt_list = [self.tile_list[25], self.tile_list[40]]
         self.path_list = [self.tile_list[13], self.tile_list[14], self.tile_list[15], self.tile_list[25], self.tile_list[27], self.tile_list[37], self.tile_list[38], self.tile_list[39]]
-        self.wall_list = [self.tile_list[127]]
+        self.wall_img = self.tile_list[126]
+        self.token_img = self.tile_list[93]
+        self.token_img = pygame.transform.scale(self.token_img, (32, 32))
 
+        #Bullet
+        self.bullet_image = pygame.image.load('spritesheet/bullet_sprite.png')
+        self.bullet_image = pygame.transform.scale(self.bullet_image, (35, 35))
+        self.bullet_image_list = [self.bullet_image, pygame.transform.rotate(self.bullet_image, 90), pygame.transform.rotate(self.bullet_image, 180), pygame.transform.rotate(self.bullet_image, 270)]
+        self.bullet_pos_list = []
         #Explosion
         explosion_sheet = SpriteSheet('spritesheet/explosion.png')
         self.explosion_list = []
@@ -58,7 +66,7 @@ class Game:
         player_x = 57
         player_y = 43
         player_sheet = SpriteSheet('spritesheet/spritesheet_characters.png')
-        self.player_image = player_sheet.get_image(0, 0, player_x, player_y)
+        self.player_image = player_sheet.get_image(0, 0, player_x, player_y, None)
         self.player_image.set_colorkey(BLACK)
         self.player_image_list = [self.player_image, pygame.transform.rotate(self.player_image, 90), pygame.transform.rotate(self.player_image, 180), pygame.transform.rotate(self.player_image, 270)]
 
@@ -72,40 +80,85 @@ class Game:
     def new(self):
         '''Create all game objects, sprites, and groups'''
         '''Call run() method'''
-        self.player = Player(self.screen, self.player_image_list)
+        self.randx = random.randint(64, DISPLAY_WIDTH-64)
+        self.randy = random.randint(64, DISPLAY_HEIGHT-64)
 
-        self.run()
+        #sprite groups
+        self.wall_sprite = pygame.sprite.Group()
+        self.token_sprite = pygame.sprite.Group()
+        self.bullet_sprite = pygame.sprite.Group()
+        self.all_sprites = pygame.sprite.Group()
 
+        #player sprites
+        self.player = Player(self.screen, 175, 75, self.player_image_list, self)
+        
 
-    def update(self):
-        '''Run all updates'''
-        self.player.get_keys()
+        #token sprites
+        self.token = Token(self.screen, self.randx, self.randy, self.token_img)
+        
+        #bullet sprites
+        # self.bullet = Bullet(self.screen, self.player.rect.centerx - 30, self.player.rect.centery - 10, self.bullet_image_list)
+        
 
-    def draw(self):
-        '''Fill the screen, draw the objets and flip'''
-
-        self.screen.fill(WHITE)
-     
         for row_index, row in enumerate(LAYOUTS[0]):
             for col_index, tile in enumerate(row):
 
                 x_loc = col_index * TILESIZE
                 y_loc = row_index * TILESIZE 
 
+                self.wall_list = []
+                if tile == '1': 
+                    brick = Wall(self.screen, x_loc, y_loc, self.wall_img)
+                    self.wall_sprite.add(brick)
+                    self.all_sprites.add(brick)
+
                 if tile == '0':
-                    self.screen.blit(self.dirt_list[1], (x_loc, y_loc))
+                    dirt_path = Background(self.screen, x_loc, y_loc, self.dirt_list[1])
+                    self.all_sprites.add(dirt_path)
 
-                elif tile == '2':
-                    self.screen.blit(self.grass_list[1], (x_loc, y_loc))
+                if tile == ' ':
+                    grass_tile = Background(self.screen, x_loc, y_loc, self.grass_list[1])
+                    self.all_sprites.add(grass_tile)
 
-                elif tile == '3':
-                    self.screen.blit(self.grass_list[2], (x_loc, y_loc))
-
-                elif tile == '1':
-                    self.screen.blit(self.wall_list[0], (x_loc, y_loc))
-                
         
-        self.player.draw()
+        #add sprites to groups
+        self.all_sprites.add(self.player)
+
+        self.all_sprites.add(self.token)
+        self.token_sprite.add(self.token)
+
+
+        self.pov = Camera(MAP_WIDTH, MAP_HEIGHT)
+
+        self.run()
+
+
+    def update(self):
+        '''Run all updates'''
+        self.player.update()
+        
+        self.score = self.player.collide_with_token()
+        
+        self.pov.update(self.player)
+
+    
+
+    def draw(self):
+        '''Fill the screen, draw the objets and flip'''
+
+        self.screen.fill(WHITE)
+
+        # camera requires blitting rather than calling all_sprites draw method
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, self.pov.get_view(sprite))
+
+
+
+        font = pygame.font.SysFont('Calibri', 35, True, False)
+        score_txt = f'Score: {self.score}'
+        score_img = font.render(score_txt, True, WHITE)
+        self.screen.blit(score_img, [260, 60])
+
         pygame.display.flip()
 
     def events(self):
@@ -116,10 +169,13 @@ class Game:
                 
                 if self.playing:
                     self.playing = False
-
                 self.running = False
 
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
 
+                    # self.player.shoot()
+            
     def run(self):
         '''contains main game loop'''
 
@@ -153,79 +209,3 @@ while game.running:
 
 pygame.quit()
 
-
-# def game_play():
-#     pygame.init()
-#     screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
-#     pygame.display.set_caption("Game Title")
-
-#     clock = pygame.time.Clock()
-#     playing = True
-
-#     tile_sheet = SpriteSheet('spritesheet/tilemap.png')
-#     tile_list = []
-
-#     #Map
-#     for y in range(11):
-#         for x in range(12):
-#             locx = 17 * x
-#             locy = 17 * y
-                
-#             tile_x = 16
-#             tile_y = 16
-            
-#             tile_image = tile_sheet.get_image(locx, locy, tile_x, tile_y, 4, 4)
-#             tile_image.set_colorkey(BLACK)
-#             tile_list.append(tile_image)
-
-
-
-#     grass1_img = tile_list[0]
-#     grass2_img = tile_list[60]
-
-#     #Explosion
-#     explosion_sheet = SpriteSheet('spritesheet/explosion.png')
-#     explosion_list = []
-
-#     for y in range(5):
-#         for x in range(5):
-#             locx = 64 * x
-#             locy = 64 * y 
-
-
-#             explosion_image = explosion_sheet.get_image(locx, locy, 64, 64)
-
-#             explosion_image.set_colorkey(BLACK)
-
-#             explosion_list.append(explosion_image)
-
-#     #Player 
-#     player_x = 57
-#     player_y = 43
-#     player_sheet = SpriteSheet('spritesheet/spritesheet_characters.png')
-#     player_image = player_sheet.get_image(0, 0, player_x, player_y)
-#     player_image.set_colorkey(BLACK)
-
-#     #Zombie 
-#     zombie_x = 36
-#     zombie_y = 43
-#     zombie_sheet = SpriteSheet('spritesheet/spritesheet_characters.png')
-#     zombie_image = zombie_sheet.get_image(425, 0, zombie_x, zombie_y)
-#     zombie_image.set_colorkey(BLACK)
-
-
-#     while playing:
-#         for event in pygame.event.get():
-#             #Events to end the game
-#             if event.type == pygame.QUIT:
-#                 playing = False
-
-#         screen.fill(BG_COLOR)
-
-#         screen.blit(explosion_list[0], (100, 100))
-#         screen.blit(player_image, (100, 100))
-#         screen.blit(zombie_image, (200, 100))
-#         screen.blit(grass2_img, (0, 0))
-#         pygame.display.flip()
-
-# game_play()
--- spritesheet/sprites.py
+++ spritesheet/sprites.py
@@ -1,11 +1,13 @@
 import pygame
+from settings import *
+import random
 
 class SpriteSheet():
     
     def __init__(self, filename):
         self.SpriteSheet = pygame.image.load(filename).convert()
 
-    def get_image(self, x, y, width, height, scale_x = None, scale_y = None):
+    def get_image(self, x, y, width, height, scale_x = None, scale_y = None, color_key = None):
 
         #Get the image at (x, y) on the spritesheet
         image = pygame.Surface((width, height))
@@ -14,44 +16,224 @@ class SpriteSheet():
         if scale_x and scale_y:
             image = pygame.transform.scale(image, (width * scale_x, height * scale_y))
 
+        if color_key:
+            color = BLACK
+            image.set_colorkey(color)
+
         return image
 
-class Player():
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, screen, x, y, img):
 
-    def __init__(self, screen, image_list):
-        self.image_list = image_list
-        self.player_speed = 10
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
         self.display = screen
-        self.x = 0
-        self.y = 0
+
+class Token(pygame.sprite.Sprite):
+    def __init__(self, screen, x, y, img):
+
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        self.display = screen
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, screen, x, y, image_list):
+
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image_list = image_list
+        self.image = self.image_list[0]
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        self.display = screen
+
+    def update(self):
+        self.rect.x -= 10
+
+    def get_keys(self):
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.image = self.image_list[2]
+        if keys[pygame.K_d]:
+            self.image = self.image_list[0]
+        if keys[pygame.K_w]:
+            self.image = self.image_list[1]
+        if keys[pygame.K_s]:
+            self.image = self.image_list[3]
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, screen, x, y, img):
+
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        
+        self.x = x
+        self.y = y
         self.vy = 0
         self.vx = 0
 
+        self.display = screen
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, screen, x, y, image_list, game):
+
+        pygame.sprite.Sprite.__init__(self)
+
+        self.game = game
+        self.image_list = image_list
+        self.player_speed = 5
+        self.display = screen
+
+        self.x = x
+        self.y = y
+        self.vy = 0
+        self.vx = 0
+        self.image = self.image_list[0]
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        self.scorecount = 0
+
     def get_keys(self):
+        self.vx, self.vy = 0, 0
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
             self.vx = -self.player_speed
-            self.img = self.image_list[2]
+            self.vy = 0
+            self.image = self.image_list[2]
         if keys[pygame.K_d]:
             self.vx = self.player_speed
-            self.img = self.image_list[0]
+            self.vy = 0
+            self.image = self.image_list[0]
         if keys[pygame.K_w]:
             self.vy = -self.player_speed
-            self.img = self.image_list[1]
+            self.vx = 0
+            self.image = self.image_list[1]
         if keys[pygame.K_s]:
             self.vy = self.player_speed
-            self.img = self.image_list[3]
+            self.vx = 0
+            self.image = self.image_list[3]
+
+
+    def collide_with_wall(self, dir):
+
+        if dir == 'x':
+
+            hits = pygame.sprite.spritecollide(self, self.game.wall_sprite, False)
+
+            if hits:
+                if self.vx > 0:
+                    self.x = hits[0].rect.left - self.rect.width
+                if self.vx < 0:
+                    self.x = hits[0].rect.right
+                
+                self.vx = 0
+                self.rect.x = self.x
+
+        if dir == 'y':
+
+            hits = pygame.sprite.spritecollide(self, self.game.wall_sprite, False)
+
+            if hits:
+                if self.vy > 0:
+                    self.y = hits[0].rect.top - self.rect.height
+                if self.vy < 0:
+                    self.y = hits[0].rect.bottom
+                
+                self.vy = 0
+                self.rect.y = self.y
+
+    def collide_with_token(self):
+        token_collision = pygame.sprite.spritecollide(self, self.game.token_sprite, True)
+
+        randx = random.randint(64, DISPLAY_WIDTH-64)
+        randy = random.randint(64, DISPLAY_HEIGHT-64)
         
+        if token_collision:
+            token = Token(self.display, randx, randy, self.game.token_img)
+            self.game.token_sprite.add(token)
+            self.game.all_sprites.add(token)
+
+            self.scorecount += 1
+
+        return self.scorecount
+
     def update(self):
         self.get_keys()
 
         self.x += self.vx
         self.y += self.vy
 
-    def draw(self):
+        self.rect.x = self.x
+        self.collide_with_wall('x')
+        self.rect.y = self.y
+        self.collide_with_wall('y')
 
-        self.display.blit(self.img, (self.x, self.y))
+        self.collide_with_token()
+
+    # def shoot(self):
+    #     self.get_keys()
+
+    #     bullet = Bullet(self.display, self.rect.centerx - 30, self.rect.centery - 10, [self.game.bullet_image])
+    #     self.game.bullet_sprite.add(bullet)
+    #     self.game.all_sprites.add(bullet)
+
+    #     bullet_collide = pygame.sprite.spritecollide(bullet, self.game.wall_sprite, True)
+
+
+
+
+class Camera:
+    def __init__(self, width, height):
+        # create a rectangle that encompasses the entire map
+        self.camera = pygame.Rect(0, 0, width, height)
+        self.width = width
+        self.height = height
+
+    def get_view(self, sprite_object):
+        # all sprite objects will be moved based on the camera's position
+        return sprite_object.rect.move(self.camera.topleft)
+    
+    def update(self, target):
+        # shift the tile map in the opposite directs of the target
+        # addding half the window size to keep the target in the center
+        x = -target.rect.x + DISPLAY_WIDTH // 2
+        y = -target.rect.y + DISPLAY_HEIGHT // 2
+
+        # to stop scrollling when the edge at the edge of the title map
+        # if the target moes too far left, or up, make x/y stay at 0
+        x = min(0, x)
+        y = min(0, y)
+
+        # if the target moves too far right or down, make the target stop
+        # at the width of the title map minus the width of the window
+        x = max(-1 * (self.width - DISPLAY_WIDTH), x)
+        y = max(-1 * (self.height - DISPLAY_HEIGHT), y)
+
+        self.camera = pygame.Rect(x, y, self.width, self.height)
 
 
 
