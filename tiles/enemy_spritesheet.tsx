c133f4633752a1e2dca0474e1f0df32d52ad3827
quilhar <davidtit240@gmail.com>
2025-05-06 13:32:35 -0400

    commit

---
+++ tiles/enemy_spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="enemy_spritesheet" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="180" columns="10">
+ <image source="tiny-creatures/Tilemap/tilemap.png" width="170" height="306"/>
+</tileset>
