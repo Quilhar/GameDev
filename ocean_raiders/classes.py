daa33e3d55c9e0447e8f9f501cb7cdc153c55a5c
quilhar <davidtit240@gmail.com>
2025-05-08 13:32:30 -0400

    commit

---
+++ ocean_raiders/classes.py
@@ -0,0 +1,135 @@
+import pygame
+from settings import *
+import random
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, screen, x, y, image, game):
+
+        pygame.sprite.Sprite.__init__(self)
+
+        self.game = game
+        self.image = image
+        self.display = screen
+
+        self.player_speed = 8
+        self.x = x
+        self.y = y
+        self.vy = 0
+        self.vx = 0
+        
+        
+        self.dir = 'right'
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        self.scorecount = 0
+
+    def get_keys(self):
+        self.vx, self.vy = 0, 0
+        
+        keys = pygame.key.get_pressed()
+
+        ## Player Movement
+        if keys[pygame.K_a]:
+            self.vx = -self.player_speed
+            self.vy = 0
+        if keys[pygame.K_d]:
+            self.vx = self.player_speed
+            self.vy = 0
+
+        if keys[pygame.K_w]:
+            self.vy = -self.player_speed
+            self.vx = 0
+
+        if keys[pygame.K_s]:
+            self.vy = self.player_speed
+            self.vx = 0
+
+    def update(self):
+        self.get_keys()
+        
+        # Update Player Position
+        self.x += self.vx
+        self.y += self.vy
+
+        self.rect.x = self.x
+        self.rect.y = self.y
+        
+        # Make sure player stays within the map
+        if self.rect.x < 0:
+            self.rect.x = 0
+        if self.rect.x > MAP_WIDTH - self.rect.width:
+            self.rect.x = MAP_WIDTH - self.rect.width
+        if self.rect.y < 0:
+            self.rect.y = 0
+        if self.rect.y > MAP_HEIGHT - self.rect.height:
+            self.rect.y = MAP_HEIGHT - self.rect.height
+
+    def collide_with_enemy(self):
+
+        # Check for collision with enemies
+        enemy_collision = pygame.sprite.spritecollide(self, self.game.enemy_sprites, False)
+
+        # If there is a collision end the game
+        if enemy_collision:
+            self.game.running = False
+
+        return self.game.playing
+    
+class Bullet(pygame.sprite.Sprite):
+
+    def __init__(self, screen, x, y, image, game):
+
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        self.y_bullet_speed = 10
+
+        self.display = screen
+
+        self.game = game
+
+    def update(self):
+        
+        # Update Bullet Position
+        self.rect.y -= self.y_bullet_speed
+
+        # Bullet Collision
+        self.bullet_collision()
+    
+    def bullet_collision(self):
+
+        # Check for collision with enemies
+        hit = pygame.sprite.spritecollide(self, self.game.enemy_sprites, True)
+
+        # What happens when a bullet hits an enemy
+        if hit:
+            self.game.score += 100
+            self.game.all_sprites.remove(self)
+            self.game.bullet_sprites.remove(self)
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, screen, x, y, image, game):
+
+        pygame.sprite.Sprite.__init__(self)
+
+        self.game = game
+        self.image = image
+        self.speed = 2
+        self.display = screen
+
+        self.x = x
+        self.y = y
+        self.vy = 0
+        self.vx = 0
+        
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
--- ocean_raiders/main.py
+++ ocean_raiders/main.py
@@ -10,11 +10,26 @@ class Game:
         pygame.init()
         pygame.mixer.init()
         
+        # Game Window and other basic settings
+        pygame.display.set_caption("Ocean Raiders")
         self.screen = pygame.display.set_mode((MAP_WIDTH, MAP_HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
 
+        # Font
+        font_path = 'ocean_raiders/ARCADE_I.TTF' # or wherever your font file is
+        size = 18
+        self.my_font = pygame.font.Font(font_path, size)
+
         self.load_images()
+        
+        # High score and score variables
+        self.highscore = 0
+        self.score = 0
+
+        # Random variables for when I might need them
+        self.randx = random.randint(64, MAP_WIDTH-64)
+        self.randy = random.randint(64, MAP_HEIGHT-64)
 
     def load_images(self):
         '''load and/or get images'''
@@ -24,6 +39,11 @@ class Game:
 
         # Player Image
         self.player_image = pygame.image.load('ocean_raiders/player_ship.png')
+        self.player_image = pygame.transform.scale(self.player_image, (TILESIZE, 64))
+
+        # Bullet Image
+        self.bullet_image = pygame.image.load('ocean_raiders/bullet.png')
+        self.bullet_image = pygame.transform.scale(self.bullet_image, (TILESIZE // 2, TILESIZE // 2))
         
         
 
@@ -33,46 +53,104 @@ class Game:
 
         # Sprite Groups
         self.all_sprites = pygame.sprite.Group()
+        self.bullet_sprites = pygame.sprite.Group()
         self.map_sprites = pygame.sprite.Group()
+        self.enemy_sprites = pygame.sprite.Group() 
 
-        # Create the map
+        # Finding Tiled Sprites
         for layer in self.map.visible_layers:
         
             tile_width = MAP_WIDTH // self.map.width
             tile_height = MAP_HEIGHT // self.map.height
             tile_size = min(tile_width, tile_height)
 
+            # Map Tiles
             if isinstance(layer, pytmx.TiledTileLayer):
 
-                
                 for x, y, surf in layer.tiles():
                     pos = (x * tile_size, y * tile_size)
-                    surf = pygame.transform.scale(surf, (tile_size, tile_size))
+                    surf = pygame.transform.scale(surf, (tile_size, tile_size)) 
                     
                     Tiled_Map(pos, surf, self.all_sprites)
+
+            # Objects
+            elif isinstance(layer, pytmx.TiledObjectGroup):
+                
+                for obj in layer:
+
+                    # Enemy Object
+                    if obj.name == 'enemy':
+                        self.enemy_image = pygame.transform.scale(obj.image, (obj.image.get_width() * 4, obj.image.get_height() * 4))
+                        self.enemy = Enemy(self.screen, obj.x * 4, obj.y * 4, self.enemy_image, self)
+                        self.enemy_sprites.add(self.enemy)
+                        self.all_sprites.add(self.enemy)
+
+        # Player
+        self.player = Player(self.screen, ((MAP_WIDTH // 2) - TILESIZE), ((MAP_HEIGHT // 2) + (6 * TILESIZE)), self.player_image, self)
+        self.all_sprites.add(self.player)
+
+        # Bullets
+        self.bullet = Bullet(self.screen, self.randx, self.randy, self.bullet_image, self)
 
         self.run()
 
     def update(self):
         '''run all updates'''
+
+        # Update all sprites
         self.all_sprites.update()
 
+        # Making sure the bullets are removed when they leave the screen
+        for bullet in self.bullet_sprites:
+            if bullet.rect.y <= 0 or bullet.rect.y >= MAP_HEIGHT or bullet.rect.x <= 0 or bullet.rect.x >= MAP_WIDTH:
+                self.bullet_sprites.remove(bullet)
+                self.all_sprites.remove(bullet)
+
+        #Updating the highscore
+        if self.score > self.highscore:
+            self.highscore = self.score
 
 
     def draw(self):
         '''fill the screen, draw the objects, and flip'''
+
+        # Blitting all sprites
+        for sprite in self.all_sprites:               
+            self.screen.blit(sprite.image, sprite.rect)
         
+        # Scores image
+        scores_txt = f'Score: {self.score}   High Score: {self.highscore}'
+        scores_img = self.my_font.render(scores_txt, True, WHITE)
+        self.screen.blit(scores_img, (30, 10))
 
         pygame.display.flip()
 
     def events(self):
         '''game loop events'''
         for event in pygame.event.get():
+
             # events to end the game
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+
                 if self.playing:
                     self.playing = False
-                    self.running = False
+                
+                self.running = False
+            
+            # Keydown events
+            elif event.type == pygame.KEYDOWN:
+                
+                ############## BULLET SPAWNING ##############
+                if event.key == pygame.K_SPACE:
+
+                    
+                    bulletx = self.player.rect.centerx - 6
+                    bullety = self.player.rect.centery - 5
+
+                    self.bullet = Bullet(self.screen, bulletx, bullety, self.bullet_image, self)
+                    self.bullet_sprites.add(self.bullet)
+                    self.all_sprites.add(self.bullet)
+
 
     def run(self):
         '''contains the main game loop'''
